8fb3a5426f829e42a3c9fac42eb48eb97f784b39
e-hokage777 <e.hokage777@gmail.com>
2026-04-10 12:17:18 +0000

    made visualize-clusters more SRP

--- visualize-clusters.py
+++ visualize-clusters.py
@@ -4,7 +4,7 @@
 ## visualize the clusters using matplotlib or seaborn
 import torch
 import numpy as np
-from GeoSSL.geossl.datasets import get_dataset_spec
+from GeoSSL.geossl.datasets import get_dataset_spec, EuroSATRGB, Resisc45, EuroSAT
 from scripts.gh_tile_dataset import GhanaTileDataset
 import pandas as pd
 import xarray as xr
@@ -13,19 +13,16 @@ import os
 import torchvision.transforms as T
 from tqdm import tqdm
 
-
 from GeoSSL.geossl.backbones import ResNetBackbone
 
 
 def extract_features_to_dataframe(
-    root_dir: str,
+    dataset: torch.utils.data.Dataset,
     fraction: float = 1.0,
     backbone_type: str = "resnet18",
     dataset_id: str = "eurosat_rgb",
     method: str = "simclr",
-    export_csv: str = None,
     device: str = "cuda:0",
-    tile_data=True,
 ) -> pd.DataFrame:
     """
     Extract features from images using ResNetBackbone and create a DataFrame.
@@ -38,19 +35,6 @@ def extract_features_to_dataframe(
     Returns:
         DataFrame with image names, coordinates, and feature columns
     """
-
-    # Define transform for resizing to match eurosat input size (64x64)
-    dataset_spec = get_dataset_spec(dataset_id)
-    normalize = T.Normalize(mean=dataset_spec.mean, std=dataset_spec.std)
-    transform = T.Compose(
-        [
-            T.Resize(dataset_spec.size),
-            T.CenterCrop(dataset_spec.crop_size),
-            *([T.ToTensor()] if dataset_id == "eurosat_rgb" else []),
-            normalize,
-        ]
-    )
-    dataset = GhanaTileDataset(root_dir, transforms=transform)
 
     # Sample indices if fraction < 1.0
     total = len(dataset)
@@ -81,30 +65,71 @@ def extract_features_to_dataframe(
             # Create row dict
             row = {}
 
-            if tile_data: ## if reading xarrays
-                row["image_name"] = dataset.files[idx]
-                row["x_min"] = dataset.bboxes[idx][0]
-                row["x_max"] = dataset.bboxes[idx][1]
-                row["y_min"] = dataset.bboxes[idx][2]
-                row["y_max"] = dataset.bboxes[idx][3]
-
-            else:
-                row["image_name"] = os.path.basename(dataset.imgs[idx])
-                row["label"] = dataset.imgs[idx][1]
-
             for i, f in enumerate(feature_np):
                 row[f"feature_{i}"] = f
 
             features_list.append(row)
 
     # Create DataFrame
-    df = pd.DataFrame(features_list)
-
-    # Export to CSV if specified
-    if export_csv:
-        df.to_csv(export_csv, index=False)
+    df = pd.DataFrame(features_list, index=indices)
 
     return df
+
+
+def select_dataset(dataset_id: str) -> torch.utils.data.Dataset:
+    """
+    Selects the appropriate dataset given the dataset_id (eurosat, eurosat_rgb, resisc45, ghana)
+
+    Args:
+        dataset_id (str): _description_
+
+    Raises:
+        NotImplementedError: _description_
+
+    Returns:
+        torch.utils.data.Dataset: _description_
+    """
+
+    # Define transform for resizing to match eurosat input size (64x64)
+    dataset_spec = get_dataset_spec(args.dataset_id)
+    normalize = T.Normalize(mean=dataset_spec.mean, std=dataset_spec.std)
+    transform = T.Compose(
+        [
+            T.Resize(dataset_spec.size),
+            T.CenterCrop(dataset_spec.crop_size),
+            *([T.ToTensor()] if args.dataset_id == "eurosat_rgb" else []),
+            normalize,
+        ]
+    )
+
+    if args.dataset_id == "eurosat":
+        dataset = EuroSAT(
+            root=os.path.abspath(__file__) + "/../data/eurosat",
+            download=True,
+            split="val",
+            transform=transform,
+        )
+    elif args.dataset_id == "eurosat_rgb":
+        dataset = EuroSATRGB(
+            root=os.path.abspath(__file__) + "/../data/eurosat",
+            download=True,
+            split="val",
+            transform=transform,
+        )
+    elif args.dataset_id == "resisc45":
+        dataset = Resisc45(
+            root=os.path.abspath(__file__) + "/../data/resisc45",
+            download=True,
+            split="val",
+            transform=transform,
+        )
+    elif args.dataset_id == "ghana":
+        dataset = GhanaTileDataset(root_dir=args.root_dir, transform=transform)
+    else:
+        raise NotImplementedError("Dataset not supported")
+
+    return dataset
+
 
 
 if __name__ == "__main__":
@@ -115,19 +140,30 @@ if __name__ == "__main__":
     parser.add_argument("--backbone_type", type=str, default="resnet18")
     parser.add_argument("--dataset_id", type=str, default="resisc45")
     parser.add_argument("--method", type=str, default="simclr")
-    parser.add_argument("--export_csv", type=str, default="data/features.csv")
     parser.add_argument("--device", type=str, default="cuda:0")
-    parser.add_argument("--tile_data", type=bool, default=True)
 
     args = parser.parse_args()
 
+    dataset = select_dataset(args.dataset_id)
+
     df = extract_features_to_dataframe(
-        root_dir=args.root_dir,
+        dataset,
         fraction=args.fraction,
         backbone_type=args.backbone_type,
         dataset_id=args.dataset_id,
         method=args.method,
-        export_csv=args.export_csv,
         device=args.device,
-        tile_data=args.tile_data,
     )
+
+    ## add extra features depending on dataset_id
+    if args.dataset_id == "ghana":
+        df["image_name"] = dataset.files
+        df["x_min"] = dataset.bboxes[:, 0]
+        df["x_max"] = dataset.bboxes[:, 1]
+        df["y_min"] = dataset.bboxes[:, 2]
+        df["y_max"] = dataset.bboxes[:, 3]
+    else:
+        df["image_name"] = [os.path.basename(f) for f in dataset.imgs]
+        df["label"] = [f[1] for f in dataset.imgs]
+
+    df.to_csv(args.export_csv, index=False)
