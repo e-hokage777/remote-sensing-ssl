6119bb9c71e7661649f4fbf6225e6f9dd8a3ada8
e-hokge777 <e.hokage777@gmail.com>
2026-05-01 14:20:50 +0000

    now plotting for each function possible

--- scripts/generate_predictions.py
+++ scripts/generate_predictions.py
@@ -6,6 +6,8 @@ from argparse import ArgumentParser
 from glob import glob
 import os
 from tqdm import tqdm
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 
 def get_train_test_split(df, train_files, test_files):
@@ -40,8 +42,25 @@ def get_pipeline(model):
 
 def predict(pipeline, X):
     preds = pipeline.predict(X)
-    pred_probs = pipeline.predict_proba(X)
+    # pred_probs = pipeline.predict_proba(X)
     return pd.DataFrame({"preds": preds})
+
+
+def create_confusion_matrix(y_true, y_pred, label_map, save_path):
+    label_names = [label_map[k] for k in sorted(label_map)]
+    if not os.path.exists(os.path.dirname(save_path)):
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    cm = confusion_matrix(y_true, y_pred)
+
+    y_true_names = [label_names[k] for k in y_true]
+    y_pred_names = [label_names[k] for k in y_pred]
+
+    disp = ConfusionMatrixDisplay.from_predictions(y_true_names, y_pred_names, colorbar=False, cmap="Blues")
+    # disp.plot()
+    disp.ax_.tick_params(axis="x", rotation=90, labelsize=8)
+    disp.ax_.tick_params(axis="y", labelsize=8)
+    plt.tight_layout()
+    plt.savefig(save_path)
 
 
 def parse_args():
@@ -58,6 +77,21 @@ def select_model(model_id):
         from sklearn.ensemble import RandomForestClassifier
 
         return RandomForestClassifier()
+    
+    if model_id == "svm":
+        from sklearn.svm import SVC
+
+        return SVC()
+    
+    if model_id == "knn":
+        from sklearn.neighbors import KNeighborsClassifier
+
+        return KNeighborsClassifier()
+    
+    if model_id == "logistic":
+        from sklearn.linear_model import LogisticRegression
+
+        return LogisticRegression()
 
 
 if __name__ == "__main__":
@@ -67,12 +101,13 @@ if __name__ == "__main__":
 
     train_files, test_files = get_train_test_files(args.split_dir)
 
-
     files = glob(f"{args.input_dir}/*.csv")
-    for file in tqdm(files):
+    for file in tqdm(files, unit="file", desc="Generating predictions and plots"):
         df = pd.read_csv(file)
 
         df_train, df_test = get_train_test_split(df, train_files, test_files)
+        df_train.reset_index(inplace=True)
+        df_test.reset_index(inplace=True)
 
         ## getting train and test splits
         X_train, y_train = df_train.filter(regex="^feature"), df_train["label"]
@@ -81,15 +116,31 @@ if __name__ == "__main__":
         ## fitting the pipeline
         pipeline.fit(X_train, y_train)
 
+        ## creating label map
+        label_names = df_test["image_name"].str.split("/").str[-2]
+        label_map = (
+            df_test[["label", "label_names"]]
+            .drop_duplicates()
+            .set_index("label")
+            .to_dict()
+        )["label_names"]
+        label_map = {int(k): v.replace("_ghana", "") for k, v in label_map.items()}
+
         ## predicting
         preds_df = predict(pipeline, X_test)
         preds_df["image_name"] = df_test["image_name"]
+        preds_df["label_names"] = df_test["image_name"].str.split("/").str[-2]
         preds_df["target"] = y_test
 
         output_dir = os.path.join(args.output_dir, args.model_id)
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
 
+        ## creating confusion matrix
+        fig_output_path = os.path.join(
+            "plots/evaluations", args.model_id, os.path.basename(file) + f"-{args.model_id}.png"
+        )
+        create_confusion_matrix(y_test, preds_df["preds"], label_map, fig_output_path)
 
-        output_file = os.path.join(output_dir, os.path.basename(file))  
+        output_file = os.path.join(output_dir, os.path.basename(file))
         preds_df.to_csv(output_file, index=False)
